fec6128ce3484b702c1ffadbab6c903f3d726db4
jhersonos <jherson.o.s@hotmail.com>
2017-05-08 15:45:42 -0500

    start class

---
+++ 01a.PREPARANDO NUESTRO LIENZO - SIN RESOLVER/js/script.js
@@ -0,0 +1,6 @@
+var contenedor = document.querySelector("#contenedor");
+function ampliar(){
+	contenedor.style.width = "100%";
+	contenedor.style.height= "100vh";
+	contenedor.style.margin= "0";
+}
